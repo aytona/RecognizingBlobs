17b702235d9e43d4f4b415f39eaef7144ca38ccd
aytona <christopher.aytona@gmail.com>
2015-10-26 22:12:14 -0400

    main.cpp organized functions to the bottom

--- RecognizingBlobs/RecognizingBlobs/main.cpp
+++ RecognizingBlobs/RecognizingBlobs/main.cpp
@@ -11,37 +11,13 @@ number generated in the range 1 to 100: if the random number is less than the
 percentage, the pixel belongs to some blob (that is you set the corresponding
 bit to 1); otherwise the pixel does not belong to the blob (that is you set the
 corresponding bit to 0). */
-BitArray genGrid(unsigned int rows, unsigned int cols, unsigned int percentage) {
-	BitArray grid(rows*cols);
-	grid.ClearAllBits();
-	unsigned int i = 0;
-	while (i < rows*cols) {
-		unsigned int randomNum = rand() % 100 + 1;
-		if (randomNum < percentage)
-			grid.SetBit(i);
-		i++;
-	}
-	return grid;
-}
+BitArray genGrid(unsigned int rows, unsigned int cols, unsigned int percentage);
 
 /* Create a function gridView with the following signature:
 string gridView(BitArray &grid, unsigned int rows, unsigned int cols)
 that produces a string representation of the grid in a form similar to that shown
 on the image above. */
-string gridView(BitArray &grid, unsigned int rows, unsigned int cols) {
-	string gridString;
-	for (unsigned int i = 0; i < rows; i++) {
-		for (unsigned int j = 0; j < cols; j++) {
-			unsigned int loc = i + j * cols;
-			if (grid[loc])
-				gridString += "x";
-			else
-				gridString += "-";
-		}
-		gridString += "\n";
-	}
-	return gridString;
-}
+string gridView(BitArray &grid, unsigned int rows, unsigned int cols);
 
 /* In order to count the blobs, you will create a function with the
 following signature: int blobCount(BitArray &grid, BitArray visited,
@@ -49,21 +25,15 @@ unsigned int rows, unsigned int cols)
 where grid is the representation of the image, and visited keeps track of the
 fact if your counting function has ever visited a particular element of the grid.
 Note if you mark a pixel as visited, you want to check the neighbor pixels and
-amrk them as well if they belong to the same blob. */
-int blobCount(BitArray &grid, BitArray visited, unsigned int rows, unsigned int cols) {
-	int count = 0;
-
-	return count;
-}
+mark them as well if they belong to the same blob. */
+int blobCount(BitArray &grid, BitArray visited, unsigned int rows, unsigned int cols);
 
 /* The marking of pixels belonging to blobs must be done using a
 recursive function with the following signature:
 void markBlob(BitArray &grid, BitArray &visited, unsinged int rows,
 unsigned int cols,unsigned int row, unsigned int col)
 where row, col is the particular pixel that is being visited at this time. */
-void markBlob(BitArray &grid, BitArray &visited, unsigned int rows, unsigned int cols, unsigned int row, unsigned int col) {
-
-}
+void markBlob(BitArray &grid, BitArray &visited, unsigned int rows, unsigned int cols, unsigned int row, unsigned int col);
 
 /* Your main() program must prompt the user for the number of
 rows and number of columns of a grid, and also for the percentage of the blobs,
@@ -85,4 +55,43 @@ int main() {
 	cout << gridView(genGrid(rows, cols, percentage), rows, cols);
 	system("PAUSE");
 	return 0;
+}
+
+BitArray genGrid(unsigned int rows, unsigned int cols, unsigned int percentage) {
+	BitArray grid(rows*cols);
+	grid.ClearAllBits();
+	unsigned int i = 0;
+	while (i < rows*cols) {
+		unsigned int randomNum = rand() % 100 + 1;
+		if (randomNum < percentage)
+			grid.SetBit(i);
+		i++;
+	}
+	return grid;
+}
+
+string gridView(BitArray &grid, unsigned int rows, unsigned int cols) {
+	string gridString;
+	for (unsigned int i = 0; i < rows; i++) {
+		for (unsigned int j = 0; j < cols; j++) {
+			unsigned int loc = i + j * cols;
+			if (grid[loc])
+				gridString += "x";
+			else
+				gridString += "-";
+		}
+		gridString += "\n";
+	}
+	return gridString;
+}
+
+int blobCount(BitArray &grid, BitArray visited, unsigned int rows, unsigned int cols) {
+	int count = 0;
+
+
+	return count;
+}
+
+void markBlob(BitArray &grid, BitArray &visited, unsigned int rows, unsigned int cols, unsigned int row, unsigned int col) {
+
 }
